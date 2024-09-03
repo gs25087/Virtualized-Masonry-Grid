62db9dd70dac2c01be6846c702ba9c281e106dd1
Gintare Simutyte <gintaresimutyte@Gintares-MacBook-Pro.local>
2024-09-03 10:46:47 +0200

    Check if response has less photos than per page constant

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,42 @@ import MasonryGrid from "./assets/components/MasonryGrid/MasonryGrid";
 import { api } from "./config/apiConfig";
 import { IPhoto } from "./types";
 
+const PER_PAGE = 10;
+
 const App: React.FC = () => {
 	const [data, setPhotosResponse] = useState<IPhoto[]>([]);
 	const [pageNumber, setPageNumber] = useState<number>(1);
 	const [isLoading, setLoading] = useState<boolean>(false);
 	const [error, setError] = useState<string | null>(null);
 	const initialFetchDone = useRef<boolean>(false);
+	const [hasMore, setHasMore] = useState<boolean>(true);
 
 	const fetchPhotos = useCallback(async () => {
-		if (isLoading) return;
+		if (isLoading || !hasMore) return;
 
 		setLoading(true);
 		setError(null);
 
 		try {
 			console.log("Fetching photos...");
+
 			const result = await api.search.getPhotos({
-				query: "waterfall costa rica",
+				query: "garden",
 				page: pageNumber,
-				perPage: 10,
+				perPage: PER_PAGE,
 			});
 
 			const newPhotos = result?.response?.results || [];
 
 			setPhotosResponse((prevPhotos) => [...prevPhotos, ...newPhotos]);
 			setPageNumber((prev) => prev + 1);
+			setHasMore(newPhotos.length === PER_PAGE);
 		} catch (err) {
 			setError(err instanceof Error ? err.message : "An unknown error occurred");
 		} finally {
 			setLoading(false);
 		}
-	}, [pageNumber, isLoading]);
+	}, [pageNumber, isLoading, hasMore]);
 
 	useEffect(() => {
 		if (!initialFetchDone.current) {
